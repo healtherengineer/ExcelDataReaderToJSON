0472efd09634f5d1dcc5d89b1a3f93900be1b031
Ege Barisan <ege.barisan.01@gmail.com>
2023-03-23 17:18:16 +0300

    edited keys and values for github

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Modülleri ekledim
+//Moduls added
 const express = require("express");
 
 const app = express();
@@ -7,13 +7,14 @@ const cors = require('cors');
 const fs = require('fs');
 const reader = require('xlsx');
 //DB Connection module
+
 const { Client } = require('pg');
 
 const client = new Client({
   user: 'postgres',
   host: 'localhost',
-  database: 'inavitas_device',
-  password: '12345',
+  database: 'sample_database',
+  password: '*****',
   port: 5432
 })
 client.connect(function(err) {
@@ -21,23 +22,7 @@ client.connect(function(err) {
   console.log("Connected!");
 });
 
-
-const query = `
-Select * from device_point ';
-`;
-
-client.query(query, (err, res) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log('Data is pulled from inavitas_device');
-    console.log(res);
-    client.end();
-});
-
-//adssa
-//npm i xlsx
+//You can write you queries now.
 
 app.use('/publicfiles', express.static(__dirname + '/publicfiles'));
 app.use(express.static(__dirname + '/public'));
@@ -48,7 +33,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 100000 }));
 
 
-//anlamadım burayı
+
 app.post("/upload", (req, res) => {
 
 
@@ -73,7 +58,8 @@ app.post("/upload", (req, res) => {
 
 })
 
-//http://localhost:3000/readexcelfile?filename=financial
+//http://localhost:3000/readexcelfile?filename=Financial
+
 app.get("/readexcelfile", (req, res) => {
 
     let fileName = req.query.filename;
@@ -86,32 +72,25 @@ app.get("/readexcelfile", (req, res) => {
         //-1 dedik kaç tane boş sayfa varsa diye bir algoritma yazıp çıkarırız baştan
         for (let i = 0; i < sheetNames.length; i++) {
             const arr = reader.utils.sheet_to_json(file.Sheets[sheetNames[i]])
-            for(let k = 0 ; k < arr.length ; k++){
-                data.push(arr[k]);
-            }
-            /*arr.forEach((res) => {
+          //  for(let k = 0 ; k < arr.length ; k++){
+            //    data.push(arr[k]);
+            //}
+            arr.forEach((res) => {
 
                 data.push(res)
 
-            })*/
+            })
         }
-        let data2array=[];
+        
 
-        data.forEach((x)=>{
-            if(Object.keys(x).includes("Tarih")){
-                data2array.push(x)
-
-            }
-        });
-        console.log(data2array)
-        res.send(data2array);
-        res.send(data);
+      res.send(data)
 
     } catch (err) {
         res.send(err);
 
     }
 })
+
 
 app.listen(3000, function () {
     console.log("node app is running on port 3000!");
