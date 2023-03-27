f8c3c7f0880c69b057797ded07e2db8ad69d93d1
Musa Beytekin <94857661+musabeytekin@users.noreply.github.com>
2023-03-27 20:08:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,8 +29,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(cors({
     origin: '*'
 }));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 100000 }));
+
+app.use(express.urlencoded({extended: true, limit: '50mb', parameterLimit: 100000}));
+app.use(express.json({ limit: '50mb' }));
+
 
 
 
